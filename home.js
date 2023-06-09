465827c0091d963de585603a318f5b741963661c
Heet Patel <hiheet04@gmail.com>
2023-06-09 09:58:55 +0530

    details added

--- home.js
+++ home.js
@@ -4,7 +4,21 @@ const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const genreURL = 'https://api.themoviedb.org/3/genre/movie/list?'+API_KEY;
 const searchURL = BASE_URL + '/search/movie?';
-if(1){
+const videoURL = 'https://api.themoviedb.org/3/movie/'
+
+
+    // $(document).ready(
+    //     function(){for(var i=1; i<=15; i++){
+    //         $('.thumbnail').click(f(i));
+    //     }
+    // }    
+    // )
+    f2 = function(){
+        $('#clickdetail').css('visibility','hidden');
+        $('#clickdetailchild').css('visibility','hidden');
+    }
+
+    var pwidth=$("#content" + 1 + " .rating").width();
     var genrelist;
     var glist;
     const getData2 = async () => {
@@ -22,9 +36,9 @@ if(1){
                 glist = Object.assign({}, ...mapped );
             }
             )
-            .catch(err => console.log('rejected', err.message));
+        .catch(err => console.log('rejected', err.message));
 
-
+//genrelist fetched
 
     const getData1 = async () => {
         const response = await fetch(API_URL);
@@ -54,11 +68,86 @@ if(1){
                     $('#content' + i).css("background-image","url(" + IMG_URL + data.results[i-1].poster_path + "), linear-gradient(180deg, rgba(29, 29, 29, 0) 0%, rgba(29, 29, 29, 0.8) 80.79%)");
                     $('#content' + i).css("background-size", "cover");
                     $('#content' + i).css("background-blend-mode", "overlay");
-                    var w = data.results[i-1].vote_average*$("#content" + i + " .rating").width()/10;
+                    var w = data.results[i-1].vote_average*pwidth/10;
                     $('#content' + i + ' .rating').css("width", w);
-        
-                    }    
+                    
+                    } 
+                    
+            for(var j=1; j<=15; j++){
+                    f = function(j){
+                        $('#clickdetail').css("visibility","visible");
+                        $('#clickdetailchild').css("visibility","visible");
+                        $('#clickdetailchild .bg').css("background-image","url(" + IMG_URL + data.results[j-1].poster_path + ")");
+                        $('#clickdetailchild .bg').css("background-size", "cover");
+                        $('#clickdetailchild .name p').text(data.results[j-1].title);
+                        $('#clickdetailchild .rating p').text("Rating: " + data.results[j-1].vote_average);
+                        $('#clickdetailchild .overview p').text(data.results[j-1].overview);
+
+                    }
+            }
             }     
             )
         .catch(err => console.log('rejected:', err.message));
+
+var myinput;
+moviesearch = () => {
+    $(document).ready(function(){
+        $('#searchbar').keyup(function(){
+            myinput = $(this).val();
+
+            const getData3 = async () => {
+                const response1 = await fetch(searchURL+'query='+myinput+'&'+API_KEY);
+                if( response1.status !== 200){
+                    throw new Error("search not fetched");
+                }
+                data = response1.json();
+                return data;
+            };
+            getData3()
+                .then(
+                    data => {
+                        for(var i=1; i<=15; i++){
+                            $('#content' + i + ' .name').text(data.results[i-1].title);
+                            var allgenres = glist[data.results[i-1].genre_ids[0] ];
+                            // for(var j=1; j<data.results[i-1].genre_ids.length; j++){
+                            //     allgenres +' ' + glist[data.results[i-1].genre_ids[j] ];
+                            // }
+                            for (j in data.results[i-1].genre_ids){
+                                if(j>0){
+                                allgenres = allgenres + ',  ' + glist[data.results[i-1].genre_ids[j] ];}
+                            } 
+                            $('#content' + i + ' .genre').text(
+                                allgenres
+                            );
+                            
+                            $('#content' + i).css("background-image","url(" + IMG_URL + data.results[i-1].poster_path + "), linear-gradient(180deg, rgba(29, 29, 29, 0) 0%, rgba(29, 29, 29, 0.8) 80.79%)");
+                            $('#content' + i).css("background-size", "cover");
+                            $('#content' + i).css("background-blend-mode", "overlay");
+                            var w = data.results[i-1].vote_average*pwidth/10;
+                            $('#content' + i + ' .rating').css("width", w);
+                            
+
+                            }   
+                            
+                            for(var j=1; j<=15; j++){
+                                f = function(j){
+                                    $('#clickdetail').css("visibility","visible");
+                                    $('#clickdetailchild').css("visibility","visible");
+                                    $('#clickdetailchild .bg').css("background-image","url(" + IMG_URL + data.results[j-1].poster_path + ")");
+                                    $('#clickdetailchild .bg').css("background-size", "cover");
+                                    $('#clickdetailchild').css("background-blend-mode", "overlay");
+                                    $('#clickdetailchild .name p').text(data.results[j-1].title);
+                                    $('#clickdetailchild .rating p').text("Rating: " + data.results[j-1].vote_average);
+                                    $('#clickdetailchild .overview p').text(data.results[j-1].overview);
+                                }
+                        }
+
+                    }
+                    )
+                    .catch(err => console.log('rejected', err.message));
+        });
+    })
 }
+
+
+
